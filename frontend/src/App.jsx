c31d2b9c9285515ffe7f167698947e522576ce7e
Guilherme Domingues <guipaula468@gmail.com>
2026-08-12 20:40:51 -0300

    feat: adiciona formulário para criar novas tarefas via POST

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import './App.css';
 function App(){
 
   const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
 
   useEffect(() => {
     fetch('http://localhost:8080/tasks')
@@ -13,11 +15,38 @@ function App(){
       .then(data => setTasks(data));
   }, []);
 
+
+  function hadlerCreateTask(){
+    fetch('http://localhost:8080/tasks', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title: newTask, status:'to-do'})
+    })
+
+      .then(response => response.json())
+      .then(createdTask => {
+        setTasks([...tasks, createdTask]);
+        setNewTask('');
+      })
+  }
+
     return(
+
       <div className='board'>
+
+        <div>
+          <input
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+            placeholder='Nova Tarefa...'
+          />
+          <button onClick={hadlerCreateTask}>Adicionar!</button>
+        </div>
+
         <Column title="To-do" tasks={tasks.filter(task => task.status === 'to-do')}/>
         <Column title="Em progresso" tasks={tasks.filter(task => task.status === 'in_progress')}/>
         <Column title="Concluído" tasks={tasks.filter(task => task.status === 'done')}/>
+
       </div>
     );
 }
